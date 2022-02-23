7f352c95ccaef06802546f2669b7b410b7328d37
zhenshan <zhenshan@belmont.tech>
2022-02-23 14:17:16 -0600

    add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import tempfile
+import pathlib
+import zipfile
+import os
+
+root_folder = tempfile.gettempdir()
+
+def unzip_model(zip_model_path):
+    p = pathlib.Path(zip_model_path)
+    # root = str(p.parents[0])
+    print(f'start unzip: {root_folder}')
+    model_path = os.path.join(root_folder, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
+    if not os.path.isdir(model_path):
+        with zipfile.ZipFile(zip_model_path, "r") as zip_ref:
+            zip_ref.extractall(root_folder)
+        
+    print(f"finish zipping model to {model_path}")
+    return model_path
+
+zip_file_path = "/pebble_tmp/tmp/test.zip"
+model_path = unzip_model(zip_file_path)
+
+def compute(i):
+    return 1
+
+
+    
