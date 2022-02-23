8c112ef7b92d287ddde2e7899ba0b1871651d3c3
zhenshan <zhenshan@belmont.tech>
2022-02-23 15:14:43 -0600

    g

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def unzip_model(zip_model_path):
     # root = str(p.parents[0])
     print(f'start unzip: {root_folder}')
     model_path = os.path.join(root_folder, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
+    print(os.listdir("/tmp"))
     if not os.path.isdir(model_path):
         print("no directory")
         print(f'create: {model_path}')
