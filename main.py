73cf056a6be9dae56975df22608d288662eb459f
zhenshan <zhenshan@belmont.tech>
2022-02-23 14:35:23 -0600

    fsd

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ def unzip_model(zip_model_path):
     # root = str(p.parents[0])
     print(f'start unzip: {root_folder}')
     model_path = os.path.join(root_folder, os.path.splitext(os.path.basename(zip_model_path))[0] + '/')
-    if not os.path.isdir(model_path):
-        with zipfile.ZipFile(zip_model_path, "r") as zip_ref:
-            zip_ref.extractall(model_path)
-        
+    with zipfile.ZipFile(zip_model_path, "r") as zip_ref:
+        zip_ref.extractall(model_path)
+
     print(f"finish zipping model to {model_path}")
     return model_path
 
 zip_file_path = "/pebble_tmp/tmp/test.zip"
-model_path = unzip_model(zip_file_path)
 
 def compute(i):
+    model_path = unzip_model(zip_file_path)
     print(root_folder)
     return model_path
 
