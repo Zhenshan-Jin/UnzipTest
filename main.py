ab57efb25a3426989fa90b82a4d0725b17286ba1
zhenshan <zhenshan@belmont.tech>
2022-03-09 15:11:49 -0600

    missing requirements

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import nltk
 from nltk.corpus import wordnet as wn
-import tempfile
+# import tempfile
 
-root_folder = tempfile.gettempdir()
-nltk.download('wordnet', download_dir=root_folder) # download the package to specific directory
-nltk.data.path.append(root_folder) # add the directory to the nltk data loading path
-nltk.download('omw-1.4', download_dir=root_folder) # download the package to specific directory
+# root_folder = tempfile.gettempdir()
+# nltk.download('wordnet', download_dir=root_folder) # download the package to specific directory
+# nltk.data.path.append(root_folder) # add the directory to the nltk data loading path
+# nltk.download('omw-1.4', download_dir=root_folder) # download the package to specific directory
 
 def compute(test):
     return {"result": [i.name() for i in wn.synsets("same")]}
